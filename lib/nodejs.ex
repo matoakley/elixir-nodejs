825e1b9af7fbe149707f8d609cf1999a532c6cd3
Matt Oakley <matt.oakley@startupheroes.co.uk>
2019-12-13 21:35:37 +0000

    Add child_spec for Supervisor

--- lib/nodejs.ex
+++ lib/nodejs.ex
@@ -3,4 +3,13 @@ defmodule NodeJS do
   def stop(), do: NodeJS.Supervisor.stop()
   def call(module, args \\ [], opts \\ []), do: NodeJS.Supervisor.call(module, args, opts)
   def call!(module, args \\ [], opts \\ []), do: NodeJS.Supervisor.call!(module, args, opts)
+  def child_spec(opts) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [opts]},
+      type: :worker,
+      restart: :permanent,
+      shutdown: 500
+    }
+  end
 end
